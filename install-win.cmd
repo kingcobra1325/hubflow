0bb740af9cc00205d2303014688915c6db5caa02
John Earl Cobar <kingcobra1325@protonmail.com>
2025-06-25 11:51:40 +0800

    fixed install win.cmd command

--- install-win.cmd
+++ install-win.cmd
@@ -1,49 +1,39 @@
 @echo off
 setlocal
-if not "%~1"=="" set GIT_HOME=%~f1
-if "%GIT_HOME%"=="" call :FindGitHome "git.cmd"
 
-if exist "%GIT_HOME%" goto :GitHomeOK
-
-echo MsysGit installation directory not found.>&2
-echo Try to give the directory name on the command line:>&2
-echo   %0 "%ProgramFiles%\Git"
-endlocal
-exit /B 1
-
-:GitHomeOK
-set ERR=0
-
-echo Installing hubflow into "%GIT_HOME%"...
-
-call :ChkGetopt getopt.exe || set ERR=1
-if %ERR%==1 goto :End
-echo getopt.exe... Found
+set GIT_HOME=C:\Program Files\Git
 
 if not exist "%GIT_HOME%\bin\git-hf" goto :Install
+
 echo HubFlow is already installed.>&2
-set /p mychoice="Do you want to replace it [y/n]"
+set /p mychoice="Do you want to replace it [y/n] "
 if "%mychoice%"=="y" goto :DeleteOldFiles
 goto :Abort
 
 :DeleteOldFiles
 echo Deleting old files...
-for /F %%i in ("%GIT_HOME%\git-hf*" "%GIT_HOME%\hubflow-*") do if exist "%%~fi" del /F /Q "%%~fi"
+for /F %%i in ("%GIT_HOME%\git-hf*" "%GIT_HOME%\hubflow-*") do (
+    if exist "%%~fi" del /F /Q "%%~fi"
+)
 
 :Install
 echo Copying files...
 ::goto :EOF
-xcopy "%~dp0\git-hf"            "%GIT_HOME%\bin"                 /Y /R /F
-if errorlevel 4 if not errorlevel 5 goto :AccessDenied
-if errorlevel 1 set ERR=1
-xcopy "%~dp0\git-hf*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
-xcopy "%~dp0\hubflow-*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
+xcopy "%~dp0\git-hf" "%GIT_HOME%\bin" /Y /R /F
+echo "%~dp0\git-hf*"
+xcopy "%~dp0\git-hf*" "%GIT_HOME%\bin" /Y /R /F || set ERR=1
+xcopy "%~dp0\hubflow-*" "%GIT_HOME%\bin" /Y /R /F || set ERR=1
 xcopy "%~dp0\shFlags\src\shflags" "%GIT_HOME%\bin\hubflow-shFlags" /Y /R /F || set ERR=1
 
-if %ERR%==1 choice /T 30 /C Y /D Y /M "Some unexpected errors happened. Sorry, you'll have to fix them by yourself."
+set "PATH=%PATH%;%GIT_HOME%\bin"
+
+if %ERR%==1 (
+    choice /T 30 /C Y /D Y /M "Some unexpected errors happened. Sorry, you'll have to fix them by yourself."
+)
 
 :End
 endlocal & exit /B %ERR%
+
 goto :EOF
 
 :AccessDenied
@@ -65,6 +55,7 @@ goto :End
 if exist "%GIT_HOME%\bin\%1" goto :EOF
 if exist "%USERPROFILE%\bin\%1" goto :EOF
 if exist "%~f$PATH:1" goto :EOF
+
 echo %GIT_HOME%\bin\%1 not found.>&2
 echo You have to install this file manually. See the GitFlow README.
 exit /B 1
@@ -72,6 +63,12 @@ exit /B 1
 :FindGitHome
 setlocal
 set GIT_CMD_DIR=%~dp$PATH:1
-if "%GIT_CMD_DIR%"=="" endlocal & goto :EOF
-endlocal & set GIT_HOME=%GIT_CMD_DIR:~0,-5%
+
+if "%GIT_CMD_DIR%"=="" (
+    endlocal
+    goto :EOF
+)
+
+endlocal
+set GIT_HOME=%GIT_CMD_DIR:~0,-5%
 goto :EOF
